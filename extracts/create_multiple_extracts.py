314e7716107965ac6b0b0975202ca82a9ffc31d1
Rainer Kujala <rainer.kujala@gmail.com>
2017-11-23 16:59:20 +0200

    Add possibility for interactive plotting of extract_start_dates w.r.t. trips per day

--- extracts/create_multiple_extracts.py
+++ extracts/create_multiple_extracts.py
@@ -1,37 +1,54 @@
 import os
 import subprocess
-from read_to_publish_csv import get_to_publish_csv
+
+from extract_pipeline import ExtractPipeline
+from read_to_publish_csv import to_publish_generator
 
 """
-This short script has one simple task: to act as a parent process when importing multiple feeds in one go.
+This script acts as a parent process when importing multiple feeds in one go.
 """
 
-ALL_CITIES = ["detroit", "lisbon", "luxembourg", "prague", "adelaide", "antofagasta", "athens", "belfast",
-              "berlin", "bilbao", "bordeaux", "brisbane", "canberra", "dublin", "melbourne", "grenoble",
-              "nantes", "palermo", "mallorca", "paris", "rennes", "rio_de_janeiro", "rome", "sydney",
+ALL_CITIES = ["adelaide", "antofagasta", "athens", "belfast",
+              "berlin", "bordeaux", "brisbane", "canberra", "detroit", "dublin", "lisbon", "luxembourg", "melbourne", "grenoble",
+              "nantes", "palermo", "prague", "mallorca", "paris", "rennes", "rio_de_janeiro", "rome", "sydney",
               "toulouse", "valencia", "valparaiso", "venice", "winnipeg", "helsinki"]
+
 def main():
     # cities_to_import = [city for city in all_cities if city not in cities_to_neglect]
 
-    # start_from = "rio_de_janeiro"
-    # cities_to_import = all_cities[all_cities.index(start_from):]
+    start_from = "bilbao"
+    cities_to_import = ALL_CITIES[ALL_CITIES.index(start_from):]
     
-    cities_to_import = ALL_CITIES # ["melbourne"]
-    commands = ["clear", "full"]
+    # cities_to_import = ALL_CITIES  # ["melbourne"]
+    commands = ["extract_start_date"]  # "clear", "full"]
 
     print("Cities to import: ", cities_to_import)
     logfile_path = get_logfile_base()
     buffer_by_line = 1
     for command in commands:
         for city_id in cities_to_import:
-            print(city_id)
+            print_dates_for_a_city(city_id)
+            # print(city_id)
             # copy_from_hammer(city_id)
             # continue
+            # standard processes
+            continue
             with open(logfile_path + "_" + city_id + ".txt", 'w', buffer_by_line) as logfile:
                 p = subprocess.Popen(['python', 'extract_pipeline.py', command, str(city_id)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                 for line in iter(p.stdout.readline, b''):
                     logfile.write(str(line) + "\n")
                     print(str(line))
+
+
+def print_dates_for_a_city(city):
+    for to_publish_tuple, feeds in list(to_publish_generator()):
+        if to_publish_tuple.id == city:
+            try:
+                pipeline = ExtractPipeline(to_publish_tuple, feeds)
+                pipeline.plot_weekly_extract_start_and_download_dates()
+            except:
+                print("Something went wrong with city " + city)
+
 
 def copy_from_hammer(city_id):
     copy_dir_name = os.path.join("copies_from_hammer", city_id)
@@ -55,8 +72,6 @@ def get_logfile_base():
     print("Logfile bases: " + logfile_path)
     return logfile_path
 
-
-
-
 if __name__ == "__main__":
     main()
+
